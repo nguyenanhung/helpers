d35311028c61ca6f7f667d584d1f4dd72e771c95
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2019-04-08 09:01:34 +0700

    Update Repo

--- src/ProjectInterface.php
+++ src/ProjectInterface.php
@@ -19,8 +19,8 @@ if (!interface_exists('nguyenanhung\Classes\Helper\ProjectInterface')) {
      */
     interface ProjectInterface
     {
-        const VERSION       = '1.0.4';
-        const LAST_MODIFIED = '2019-03-25';
+        const VERSION       = '1.0.5';
+        const LAST_MODIFIED = '2019-04-08';
         const AUTHOR_NAME   = 'Hung Nguyen';
         const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
         const PROJECT_NAME  = 'Helper';
