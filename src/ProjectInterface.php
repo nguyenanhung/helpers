b26f6a83180d3088c5ce0514d6279aa00d9a1cf1
nguyenanhung <info@nguyenanhung.com>
2019-09-13 10:38:44 +0700

    Update helper

--- src/ProjectInterface.php
+++ src/ProjectInterface.php
@@ -19,8 +19,8 @@ if (!interface_exists('nguyenanhung\Classes\Helper\ProjectInterface')) {
      */
     interface ProjectInterface
     {
-        const VERSION       = '1.0.9';
-        const LAST_MODIFIED = '2019-07-21';
+        const VERSION       = '1.0.10';
+        const LAST_MODIFIED = '2019-09-13';
         const AUTHOR_NAME   = 'Hung Nguyen';
         const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
         const PROJECT_NAME  = 'Helper';
