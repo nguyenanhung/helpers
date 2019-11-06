17c4c137e7b3d4eafff372b506816747efe2cf73
nguyenanhung <info@nguyenanhung.com>
2019-11-06 11:01:13 +0700

    deploy new version

---
+++ src/helpers/email_helper.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Project helpers
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 11/6/19
+ * Time: 10:51
+ */
+if (!function_exists('isEmail')) {
+    /**
+     * Function isEmail
+     *
+     * @param string $mail
+     *
+     * @return bool
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 11/6/19 52:32
+     */
+    function isEmail($mail = '')
+    {
+        return filter_var($mail, FILTER_VALIDATE_EMAIL) ? TRUE : FALSE;
+    }
+}
