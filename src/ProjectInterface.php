a8eae808ee17a3797ca61a450ddd58492ce60f9f
nguyenanhung <info@nguyenanhung.com>
2020-02-11 12:23:20 +0700

    Update filter helper

--- src/ProjectInterface.php
+++ src/ProjectInterface.php
@@ -19,8 +19,8 @@ if (!interface_exists('nguyenanhung\Classes\Helper\ProjectInterface')) {
      */
     interface ProjectInterface
     {
-        const VERSION       = '1.1.5';
-        const LAST_MODIFIED = '2020-01-14';
+        const VERSION       = '1.1.6';
+        const LAST_MODIFIED = '2020-02-11';
         const AUTHOR_NAME   = 'Hung Nguyen';
         const AUTHOR_EMAIL  = 'dev@nguyenanhung.com';
         const PROJECT_NAME  = 'Helper';
