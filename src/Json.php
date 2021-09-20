dfba9cfe3e7d61b111805f2b6173c5fe54625dcb
nguyenanhung <info@nguyenanhung.com>
2021-09-21 01:26:38 +0700

    Update json and xml

--- src/Json.php
+++ src/Json.php
@@ -10,6 +10,8 @@
 
 namespace nguyenanhung\Classes\Helper;
 
+use nguyenanhung\Libraries\JSON\Json as BaseJson;
+
 if (!class_exists('nguyenanhung\Classes\Helper\Json')) {
     /**
      * Class Json
@@ -18,45 +20,7 @@ if (!class_exists('nguyenanhung\Classes\Helper\Json')) {
      * @author    713uk13m <dev@nguyenanhung.com>
      * @copyright 713uk13m <dev@nguyenanhung.com>
      */
-    class Json
+    class Json extends BaseJson
     {
-        /**
-         * Function jsonItem
-         *
-         * @param string $json_string
-         * @param string $item_output
-         *
-         * @return null|string
-         * @author: 713uk13m <dev@nguyenanhung.com>
-         * @time  : 9/29/18 11:08
-         *
-         */
-        public static function jsonItem($json_string = '', $item_output = ''): ?string
-        {
-            $result      = json_decode(trim($json_string));
-            $item_output = trim($item_output);
-            if (($result !== null) && isset($result->$item_output)) {
-                return trim($result->$item_output);
-            }
-
-            return NULL;
-        }
-
-        /**
-         * Function isJson
-         *
-         * @param string $json
-         *
-         * @return bool TRUE or FALSE
-         * @author: 713uk13m <dev@nguyenanhung.com>
-         * @time  : 10/13/18 09:39
-         *
-         */
-        public static function isJson($json = ''): bool
-        {
-            $decode = json_decode(trim($json));
-
-            return !($decode === null);
-        }
     }
 }
