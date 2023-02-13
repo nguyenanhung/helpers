b8acfb2381e31a9bfae7b61057388937a97293e4
nguyenanhung <info@nguyenanhung.com>
2023-02-14 00:36:44 +0700

    Release version 3.1.3.1

--- src/ProjectInterface.php
+++ src/ProjectInterface.php
@@ -19,7 +19,7 @@ if (!interface_exists('nguyenanhung\Classes\Helper\ProjectInterface')) {
      */
     interface ProjectInterface
     {
-        const VERSION = '3.1.3';
+        const VERSION = '3.1.3.1';
         const LAST_MODIFIED = '2023-02-13';
         const AUTHOR_NAME = 'Hung Nguyen';
         const AUTHOR_EMAIL = 'dev@nguyenanhung.com';
