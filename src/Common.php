51c7e20541b0acd73bc8da509d35c308793d5b98
nguyenanhung <info@nguyenanhung.com>
2023-07-24 10:51:03 +0700

    Release next-gen version v3.x

--- src/Common.php
+++ src/Common.php
@@ -222,7 +222,7 @@ if (!class_exists('nguyenanhung\Classes\Helper\Common')) {
          * @time  : 9/29/18 11:07
          *
          */
-        public function placeholder($size = '300x250', $bg_color = '', $text_color = '', $text = '', $domain = 'https://via.placeholder.com/'): string
+        public function placeholder($size = '300x250', $bg_color = '', $text_color = '', $text = '', $domain = 'https://placehold.co/'): string
         {
             return (new HtmlCommon())->placeholder($size, $bg_color, $text_color, $text, $domain);
         }
